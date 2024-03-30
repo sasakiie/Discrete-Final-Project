d6418b9de21523af15a68968678bd5486e6f281f
phatnonz <phattharachai2547@gmail.com>
2024-03-30 20:37:29 +0700

    shortestpath&exInput

--- Edge.java
+++ Edge.java
@@ -1,168 +1,200 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Edge {
-    int src , dest , weight;
+class Edge {
+    int source, dest, weight;
 
-    public Edge(int scr , int dest , int weight){
-        this.src = scr ;
-        this.dest = dest ;
-        this.weight = weight ;
+    public Edge(int source, int dest, int weight) {
+        this.source = source;
+        this.dest = dest;
+        this.weight = weight;
     }
-    
 }
+class Graph {
+    int vertices;
+    ArrayList<Edge> edges;
 
-class Graph{
-    int V , E ;
-    List<Edge>edges;
-
-    public Graph(int  V, int E) {
-        this.V = V;
-        this.E = E;
+    public Graph(int vertices) {
+        this.vertices = vertices;
         edges = new ArrayList<>();
     }
 
-    public void addEdge(int src , int dest , int weight ){
-        edges.add(new Edge(src , dest , weight));
+    public void addEdge(int source, int dest, int weight) {
+        edges.add(new Edge(source, dest, weight));
     }
 
-    //check  if the graph is connected or not
-    public boolean isConnected(){
-        boolean[] visited = new boolean[V];
-        Arrays.fill(visited, false);
+    public boolean isContinuousGraph() {
+        boolean[] visited = new boolean[vertices];
+        dfs(0, visited);
 
-        DFS(0, visited);
-
-        //check  if all vertices are visited or not
-        for(int i = 0; i<V ;i++){
-            if(!visited[i]) return false;
+        for (boolean v : visited) {
+            if (!v) {
+                return false;
+            }
         }
+
         return true;
-
-        
     }
 
-    //Depth First Search
-    private void DFS(int v ,boolean[] visited){
-        visited[v] = true;
+    private void dfs(int vertex, boolean[] visited) {
+        visited[vertex] = true;
+
         for (Edge edge : edges) {
-            if (edge.src == v && !visited[edge.dest]) {
-                DFS(edge.dest, visited);
-            }
-            else if(edge.dest == v &&  !visited[edge.src]){
-                DFS(edge.src, visited);
-            } 
-        }
-    }
-
-    //Kruskal's algorithm
-    public List<Edge> kruskalMST(){
-        List<Edge> mst = new ArrayList<>();
-        //sort edges by weight
-        edges.sort(Comparator.comparingInt(o -> o.weight));
-
-        DisjointSet ds = new DisjointSet(V);
-        
-        for (Edge edge : edges) {
-            int srcSet = ds.find(edge.src);
-            int destSet = ds.find(edge.dest);
-
-            if (srcSet != destSet) {
-                mst.add(edge);
-                ds.union(srcSet , destSet);
+            if (edge.source == vertex && !visited[edge.dest]) {
+                dfs(edge.dest, visited);
             }
         }
-        return  mst;
     }
 
-    //Prim's Algorithm
-    public List<Edge> primMST(){
-        List<Edge> mst = new ArrayList<>();
-        boolean [] visited = new boolean[V];
-        int [] key = new int[V];
-        int [] parent = new int[V];
-        Arrays.fill(visited,false);
-        Arrays.fill(key,Integer.MAX_VALUE);
-        key[0]=0; //start from vertex 0
 
-        for (int i = 0; i < V-1; i++) {
-            int u = minKey(key,visited);
-            visited[u] = true;
-
-            for(Edge edge : edges){
-                if ((edge.src == u || edge.dest == u) && !visited[edge.dest]) {
-                    int v = (edge.src == u ) ? edge.dest : edge.src;
-                    if (edge.weight < key[v]) {
-                        parent[v] = u;
-                        key[v] = edge.weight;
-                    }
-                    
+    //prim algorithm
+    public void prim() {
+        int[] parent = new int[vertices];
+        int[] key = new int[vertices];
+        boolean[] mstSet = new boolean[vertices];
+    
+        for (int i = 0; i < vertices; i++) {
+            key[i] = Integer.MAX_VALUE;
+            mstSet[i] = false;
+        }
+    
+        key[0] = 0;
+        parent[0] = -1;
+    
+        for (int count = 0; count < vertices - 1; count++) {
+            int u = minKey(key, mstSet);
+            mstSet[u] = true;
+    
+            for (Edge edge : edges) {
+                if (edge.source == u && !mstSet[edge.dest] && edge.weight < key[edge.dest]) {
+                    parent[edge.dest] = u;
+                    key[edge.dest] = edge.weight;
                 }
             }
         }
-        
-        //construct mst
-        for (int i = 0; i < V; i++) {
-            mst.add(new Edge(parent[i], i, key[i]));
-        }
-        return mst;
+    
+        printMST(parent);
     }
-    private int minKey(int []key , boolean []visited){
-        int min = Integer.MAX_VALUE;
-        int minIndex = -1;
-        for (int v=0;v<V;v++){
-            if (!visited[v] && key[v]<min){
+    
+    private int minKey(int[] key, boolean[] mstSet) {
+        int min = Integer.MAX_VALUE, minIndex = -1;
+    
+        for (int v = 0; v < vertices; v++) {
+            if (!mstSet[v] && key[v] < min) {
                 min = key[v];
                 minIndex = v;
             }
         }
+    
         return minIndex;
     }
-
     
+    private void printMST(int[] parent) {
+        System.out.println("Edge \tWeight");
+        for (int i = 1; i < vertices; i++) {
+            System.out.println(parent[i] + " - " + i + "\t" + getEdgeWeight(parent[i], i));
+        }
+    }
+    
+    private int getEdgeWeight(int u, int v) {
+        for (Edge edge : edges) {
+            if ((edge.source == u && edge.dest == v) || (edge.source == v && edge.dest == u)) {
+                return edge.weight;
+            }
+        }
+        return -1;
+    }
+    
+    //kruska
+
+    public void kruskal() {
+        ArrayList<Edge> result = new ArrayList<>();
+        edges.sort(Comparator.comparingInt(e -> e.weight));
+    
+        int[] parent = new int[vertices];
+        for (int i = 0; i < vertices; i++) {
+            parent[i] = i;
+        }
+    
+        int e = 0, i = 0;
+        while (e < vertices - 1 && i < edges.size()) {
+            Edge nextEdge = edges.get(i++);
+            int x = find(parent, nextEdge.source);
+            int y = find(parent, nextEdge.dest);
+    
+            if (x != y) {
+                result.add(nextEdge);
+                e++;
+                union(parent, x, y);
+            }
+        }
+    
+        System.out.println("Edge \tWeight");
+        for (Edge edge : result) {
+            System.out.println(edge.source + " - " + edge.dest + "\t" + edge.weight);
+        }
+    }
+    
+    private int find(int[] parent, int i) {
+        if (parent[i] != i) {
+            parent[i] = find(parent, parent[i]);
+        }
+        return parent[i];
+    }
+    
+    private void union(int[] parent, int x, int y) {
+        int xSet = find(parent, x);
+        int ySet = find(parent, y);
+        parent[xSet] = ySet;
+    }
+    
+    
+    //shortest
+    public void shortestPathFromVertex(char startVertex) {
+        int[][] dist = new int[vertices][vertices];
+
+        for (int i = 0; i < vertices; i++) {
+            for (int j = 0; j < vertices; j++) {
+                if (i == j) {
+                    dist[i][j] = 0;
+                } else {
+                    dist[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        for (Edge edge : edges) {
+            dist[edge.source][edge.dest] = edge.weight;
+            dist[edge.dest][edge.source] = edge.weight;
+        }
+
+        for (int k = 0; k < vertices; k++) {
+            for (int i = 0; i < vertices; i++) {
+                for (int j = 0; j < vertices; j++) {
+                    if (dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE &&
+                            dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        Map<Integer, Character> reverseMapping = new HashMap<>();
+        for (int i = 0; i < vertices; i++) {
+            reverseMapping.put(i, (char) ('A' + i));
+        }
+
+        System.out.println("Shortest paths from vertex " + startVertex + ":");
+        for (int i = 0; i < vertices; i++) {
+            if (reverseMapping.get(i) != startVertex) {
+                System.out.println("To vertex " + reverseMapping.get(i) + ": " + dist[startVertex - 'A'][i]);
+            }
+        }
+    }
+    
+
+
 }
 
-class DisjointSet{
-    int  [] parent , rank ;
-    
-    public DisjointSet(int n ){
-        parent = new int [n];
-        rank = new int[n];
-        for(int i = 0; i < n; i++){
-            makeSet(i);
-        }
-    }
-
-    private void makeSet(int v){
-        parent[v] = v ;
-        rank[v] = 0;
-    }
-
-    public int find(int v){
-        if (v != parent[v]) {
-            parent[v] = find(parent[v]);
-        }
-        return parent[v];
-    }
-
-    public void union(int u , int v){
-        int rootU = find(u);
-        int rootV = find(v);
-        if (rootU != rootV) {
-            if (rank[rootU] > rank[rootV]) {
-                parent[rootV] = rootU;
-            }
-            else if(rank[rootU] < rank[rootV]){
-                parent[rootU] = rootV;
-            }
-            else{
-                parent[rootV] = rootU;
-                rank[rootU]++;
-            }          
-        }
-
-    }
-}

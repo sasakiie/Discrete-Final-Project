1d7bdba38af265112ef9832493854b57d7b224d0
Phawothai Na pairee <bright4869napairee@gmail.com>
2024-04-01 18:38:29 +0700

    add Automata class

---
+++ Automata.java
@@ -0,0 +1,54 @@
+public class Automata {
+    enum State {
+        START , STATE_1 , ACCEPT , REJECT
+    }
+
+    private State currentState;
+
+    public Automata(){
+        currentState = State.START;
+    }
+
+    public void processInput(String input){
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            switch (currentState) {
+                case START:
+                    if (currentChar == '1') {
+                        currentState = State.START;
+                    }
+                    if (currentChar == '0') {
+                        currentState = State.STATE_1;
+                    }
+                    else{
+                        currentState = State.REJECT;
+                    }
+                    break;
+                case STATE_1:
+                    if (currentChar == '0') {
+                        currentState = State.STATE_1;
+                    }
+                    if (currentChar == '1') {
+                        currentState = State.ACCEPT;
+                    }
+                    break;
+                case ACCEPT:
+                    if (currentChar == '1') {
+                        currentState = State.START;
+                    }
+                    if (currentChar == '0') {
+                        currentState = State.STATE_1;  
+                    }
+                    break;
+                case REJECT:{break;}
+            }
+            
+        }
+    }
+    
+    public boolean isAccepted(){
+        return currentState == State.ACCEPT;
+    }
+
+    
+}
--- Minimum_Spanning_Tree.java
+++ Minimum_Spanning_Tree.java
@@ -4,57 +4,165 @@ import java.util.Scanner;
 public class Minimum_Spanning_Tree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number of vertices: ");
-        int vertices = scanner.nextInt();
+        System.out.println("Select algorithm : \n\t 1. Prim's Algorithm \n\t 2. Kruskal's Algorithm \n\t 3. Shortest Path \n\t 4. Automata ");
+        System.out.print("Enter your choice : ");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.print("Enter number of vertices: ");
+                int vertices_case1 = scanner.nextInt();
+                System.out.print("Enter number of edges: ");
+                int edgesCount_case1 = scanner.nextInt();
+                Graph graph1 = new Graph(vertices_case1);
+                for(int i = 0; i < edgesCount_case1; i++){
+                    System.out.print("Enter edge " + (i + 1) + " details (source dest weight): ");
+                    String source = scanner.next();
+                    String dest = scanner.next();
+                    int weight = scanner.nextInt();
 
-        System.out.print("Enter number of edges: ");
-        int edgesCount = scanner.nextInt();
+                    // แปลง source และ dest เป็นตัวเลข
+                    int sourceIndex = source.charAt(0) - 'A';
+                    int destIndex = dest.charAt(0) - 'A';
 
-        Graph graph = new Graph(vertices);
+                    graph1.addEdge(sourceIndex, destIndex, weight);
+                }
 
-        for (int i = 0; i < edgesCount; i++) {
-            System.out.print("Enter edge " + (i + 1) + " details (source dest weight): ");
-            String source = scanner.next();
-            String dest = scanner.next();
-            int weight = scanner.nextInt();
+                if (graph1.isContinuousGraph()) {
+                    System.out.println("The graph is a connected graph.");
+                    System.out.println();
+                    graph1.prim();
+                }
+                else{
+                    System.out.println("The graph is not a connected graph.");
+                }
+                break;
 
-            // แปลง source และ dest เป็นตัวเลข
-            int sourceIndex = source.charAt(0) - 'A';
-            int destIndex = dest.charAt(0) - 'A';
+            case 2:
+                System.out.print("Enter number of vertices: ");
+                int vertices_case2 = scanner.nextInt();
+                System.out.print("Enter number of edges: ");
+                int edgesCount_case2 = scanner.nextInt();
+                Graph graph2 = new Graph(vertices_case2);
+                for(int i = 0; i < edgesCount_case2; i++){
+                    System.out.print("Enter edge " + (i + 1) + " details (source dest weight): ");
+                    String source = scanner.next();
+                    String dest = scanner.next();
+                    int weight = scanner.nextInt();
 
-            graph.addEdge(sourceIndex, destIndex, weight);
-        }
+                    // แปลง source และ dest เป็นตัวเลข
+                    int sourceIndex = source.charAt(0) - 'A';
+                    int destIndex = dest.charAt(0) - 'A';
 
-        if (graph.isContinuousGraph()) {
-            System.out.println("The graph is a connected graph.");
-            System.out.println();
+                    graph2.addEdge(sourceIndex, destIndex, weight);
+                }
 
-            // เพิ่ม switch case เพื่อให้ผู้ใช้เลือกใช้ Algorithm
-            System.out.println("Select algorithm :");
-            System.out.println("1. Prim's Algorithm");
-            System.out.println("2. Kruskal's Algorithm");
-            System.out.println("3. Shortest Path Algorithm");
-            System.out.print("Enter your choice : ");
+                if (graph2.isContinuousGraph()) {
+                    System.out.println("The graph is a connected graph.");
+                    System.out.println();
+                    graph2.kruskal();
+                }
+                else{
+                    System.out.println("The graph is not a connected graph.");
+                }
+                break;
 
-            int choice = scanner.nextInt();
-            switch (choice) {
-                case 1:
-                    graph.prim();
-                    break;
-                case 2:
-                    graph.kruskal();
-                    break;
-                case 3:
+            case 3:
+                System.out.print("Enter number of vertices: ");
+                int vertices_case3 = scanner.nextInt();
+                System.out.print("Enter number of edges: ");
+                int edgesCount_case3 = scanner.nextInt();
+                Graph graph3 = new Graph(vertices_case3);
+                for(int i = 0; i < edgesCount_case3; i++){
+                    System.out.print("Enter edge " + (i + 1) + " details (source dest weight): ");
+                    String source = scanner.next();
+                    String dest = scanner.next();
+                    int weight = scanner.nextInt();
+
+                    // แปลง source และ dest เป็นตัวเลข
+                    int sourceIndex = source.charAt(0) - 'A';
+                    int destIndex = dest.charAt(0) - 'A';
+
+                    graph3.addEdge(sourceIndex, destIndex, weight);
+                }
+
+                if (graph3.isContinuousGraph()) {
+                    System.out.println("The graph is a connected graph.");
                     System.out.print("Enter starting vertex (A-Z): ");
                     char startVertex = scanner.next().charAt(0);
-                    graph.shortestPathFromVertex(startVertex);
-                    break;
-                default:
-                    System.out.println("Invalid choice.");
+                    graph3.shortestPathFromVertex(startVertex);
+                }
+                else{
+                    System.out.println("The graph is not a connected graph.");
+                }
+                break;
+            case 4:
+                Scanner Scanner = new Scanner(System.in);
+                System.out.println("Enter the Automata Input : ");
+                String input = Scanner.nextLine();
+
+                Automata atm = new Automata();
+                atm.processInput(input);
+                if (atm.isAccepted()) {
+                    System.out.println("Accepted!");
+                }
+                else{
+                    System.out.println("Rejected!");
+                }
+                break;
+                
+           
+            default:
+                System.out.println("Invalid choice.");
             }
-        } else {
-            System.out.println("The graph is not a connected graph.");
-        }
+    //     System.out.print("Enter number of vertices: ");
+    //     int vertices = scanner.nextInt();
+
+    //     System.out.print("Enter number of edges: ");
+    //     int edgesCount = scanner.nextInt();
+
+    //     Graph graph = new Graph(vertices);
+
+    //     for (int i = 0; i < edgesCount; i++) {
+    //         System.out.print("Enter edge " + (i + 1) + " details (source dest weight): ");
+    //         String source = scanner.next();
+    //         String dest = scanner.next();
+    //         int weight = scanner.nextInt();
+
+    //         // แปลง source และ dest เป็นตัวเลข
+    //         int sourceIndex = source.charAt(0) - 'A';
+    //         int destIndex = dest.charAt(0) - 'A';
+
+    //         graph.addEdge(sourceIndex, destIndex, weight);
+    //     }
+
+    //     if (graph.isContinuousGraph()) {
+    //         System.out.println("The graph is a connected graph.");
+    //         System.out.println();
+
+    //         // เพิ่ม switch case เพื่อให้ผู้ใช้เลือกใช้ Algorithm
+    //         System.out.println("Select algorithm : \n\t 1. Prim's Algorithm \n\t 2. Kruskal's Algorithm \n\t 3. Shortest Path \n\t 4. Automata ");
+            
+    //         System.out.print("Enter your choice : ");
+
+            
+    //         switch (choice) {
+    //             case 1:
+    //                 graph.prim();
+    //                 break;
+    //             case 2:
+    //                 graph.kruskal();
+    //                 break;
+    //             case 3:
+    //                 System.out.print("Enter starting vertex (A-Z): ");
+    //                 char startVertex = scanner.next().charAt(0);
+    //                 graph.shortestPathFromVertex(startVertex);
+    //                 break;
+    //             default:
+    //                 System.out.println("Invalid choice.");
+    //         }
+    //     } else {
+    //         System.out.println("The graph is not a connected graph.");
+    //     }
 
         scanner.close();
     }
